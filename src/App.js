2550f3564bedbe430f2a08c5fed386383a612c5d
mvojnovski <vojnovskimartin>
2023-01-18 17:21:49 +0100

    api added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
+import { useEffect } from 'react';
+
+//ed418a88ed418a88
+
+const API_URL = 'http://www.omdbapi.com?apikey=ed418a88';
 
 function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch (`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+     searchMovies('Superman');
+  }, []);
+
   return (
     <div className="App">
 
